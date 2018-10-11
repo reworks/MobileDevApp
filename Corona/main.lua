65afb3e9b768e9e6114113e48682a98a56a27934
reworks <dev.dom.re@gmail.com>
2018-10-11 16:48:02 +0800

    progress on app

--- Corona/main.lua
+++ Corona/main.lua
@@ -1,5 +1,7 @@
 local library = require "plugin.library"
 
+-- Android native stuff
+
 -- This event is dispatched to the global Runtime object
 -- by `didLoadMain:` in MyCoronaDelegate.mm
 local function delegateListener( event )
@@ -22,8 +24,68 @@ timer.performWithDelay( 1000, function()
 	library.show( "corona" )
 end )
 
+-- App stuff
+local widget = require("widget")
 
--- Display the background image.
-local background = display.newImage("paper_bkg.png", true)
-background.x = display.contentCenterX
-background.y = display.contentCenterY
+-- App stuff
+local function checkForRego()
+	local file, error = io.open(system.pathForFile("reg.txt", system.DocumentsDirectory))
+
+	if not file then
+		return 0
+	else
+		local number = file:read(10)
+		io.close(file)
+		return number
+	end
+end
+
+local function displayMenu()
+end
+
+local function displayRegoInput()
+	display.newText({text = "Hello!", x = display.contentCenterX,
+	y = display.contentCenterY, font = native.systemFont, fontSize = 16})
+end
+
+local function displayRego()
+	local background = display.newImage("register.png", true)
+	background.x = display.contentCenterX
+	background.y = display.contentCenterY
+
+	local regButton = widget.newButton(
+	{
+		x = display.contentCenterX,
+		y = display.contentCenterY + 247,
+		shape = "rect",
+		fillColor = 
+		{	
+			default = 
+			{
+				1.0, 0.0, 0.0, 1.0
+			},
+			over = 
+			{
+				0.0, 0.0, 1.0, 0.2
+			}
+		},
+		width = 315,
+		height = 51,
+		isEnabled = true,
+		onRelease = displayRegoInput
+	})
+end
+
+local function main()
+	display.setStatusBar( display.HiddenStatusBar )
+	
+	local number = checkForRego()
+
+	if number == 0 then
+		displayRego()
+	else
+		displayMenu()
+	end
+end
+
+main()
